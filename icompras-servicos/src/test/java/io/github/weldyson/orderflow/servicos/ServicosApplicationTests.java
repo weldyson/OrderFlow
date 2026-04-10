902bcbc2c8784f0a1984c46430976dc726e88705
Weldyson de Azevedo <41272112+weldyson@users.noreply.github.com>
2026-04-10 14:08:15 -0300

    Initial commit

---
+++ icompras-servicos/src/test/java/io/github/weldyson/orderflow/servicos/ServicosApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.weldyson.orderflow.servicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ produtos/src/main/java/io/github/weldyson/orderflow/produtos/ProdutosApplication.java
@@ -0,0 +1,13 @@
+package io.github.weldyson.orderflow.produtos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutosApplication.class, args);
+	}
+
+}
